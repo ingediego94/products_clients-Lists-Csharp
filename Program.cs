f7088ad8334f3866ce1b5f0a46b8ed40f97640f2
ingediego94 <inge.diego94@gmail.com>
2025-09-15 19:35:23 -0500

    Second Commit

--- Program.cs
+++ Program.cs
@@ -1,61 +1,76 @@
-﻿using System.Security.Cryptography;
-
-namespace crudsazo;
+﻿namespace crudsazo;
 
 public class Program
 {
-    // Products Class:
-    public class Products
+    
+    // class to products
+    public class Products()
     {
         public int Code { get; set; }
         public string Name { get; set; }
         public double Price { get; set; }
+        public bool Status { get; set; }
     }
-
     
-    // Clients Class:
-    public class Clients
+    
+    // class to clients
+    public class Clients()
     {
         public int Id { get; set; }
-        public string NameC { get; set; }
+        public string Name_C { get; set; }
         public int Age { get; set; }
-        public Products product_x { get; set; }
+        public bool Status_C { get; set; }
+        public List<Products> ProductsList { get; set; } = new List<Products>();
     }
     
-    // Main method:
+    
+    
+    // Main class
     public static void Main(string[] args)
     {
-
-        // Creacion de los 4 productos:
+        // Option number 1:
         var prod1 = new Products();
-        prod1.Code = 11;
-        prod1.Name = "Laptop";
-        prod1.Price = 4680000.0;
-
-        var prod2 = new Products { Code = 22, Name = "Celular", Price = 845000.2 };
-        var prod3 = new Products { Code = 33, Name = "Libro", Price = 99100.1 };
-        var prod4 = new Products { Code = 44, Name = "Audifonos", Price = 55650.8 };
-        
-
-            
-        // Creacion de los 2 clientes:
-        var client1 = new Clients { Id = 898, NameC = "Susana", Age = 22, product_x = prod2 };
-        var client2 = new Clients { Id = 1999, NameC = "Sara", Age = 33, product_x = prod1};
-        var client3 = new Clients { Id = 2000, NameC = "Fernanda", Age = 18, product_x = prod3};
-        var client4 = new Clients { Id = 2100, NameC = "John", Age = 43, product_x = prod4};
-        
-
-        // // Lista de productos
-        List<Clients> clientList = new List<Clients> {client1, client2, client3, client4 };
+        prod1.Code = 21321;
+        prod1.Name = "Silla Gamer Pro";
+        prod1.Price = 834999.12;
+        prod1.Status = true;
         
         
-        // Impresion:
-        foreach (var cliente_x in clientList)
+        // Option number 2:
+        var prod2 = new Products { Code = 54341, Name = "Monitor Gamer", Price = 663999.32, Status = true };
+        var prod3 = new Products { Code = 2321, Name = "Mouse Gamer", Price = 89999.2, Status = false };
+        var prod4 = new Products { Code = 5432, Name = "Mesa de juego", Price = 787874.3, Status = true };
+        
+        
+        // Creación de clientes y sus respectivos productos
+        var client1 = new Clients { Id = 11, Name_C = "Cristina Agudelo", Age = 43, Status_C = true};
+        client1.ProductsList.Add(prod4);
+        client1.ProductsList.Add(prod2);
+        
+        var client2 = new Clients { Id = 22, Name_C = "Diego Vallejo", Age = 31, Status_C = true};
+        client2.ProductsList.Add(prod1);
+        client2.ProductsList.Add(prod3);
+        
+        var client3 = new Clients { Id = 33, Name_C = "Edison Guachucal", Age = 23, Status_C = true};
+        client3.ProductsList.Add(prod4);
+        client3.ProductsList.Add(prod1);
+        client3.ProductsList.Add(prod3);
+        
+        
+        // Lista de productos:
+        List<Clients> clientes = new List<Clients> { client1, client2, client3 };
+        
+        // Impresion con foreach
+        foreach (var cliente in clientes)
         {
-            Console.WriteLine($"Id cliente: {cliente_x.Id,5} --- Nombre: {cliente_x.NameC,7} --- Edad: {cliente_x.Age,4} --- Producto: {cliente_x.product_x.Name,10}");
+            Console.WriteLine($"El cliente con ID: {cliente.Id}, nombre: {cliente.Name_C}, Edad: {cliente.Age}, Estado: {cliente.Status_C}");
+            Console.WriteLine("   Productos comprados:");
+            foreach (var producto in cliente.ProductsList)
+            {
+                Console.WriteLine($"      - {producto.Name} (${producto.Price})");
+            }
         }
-
-
-
     }
+    
+    
 }
